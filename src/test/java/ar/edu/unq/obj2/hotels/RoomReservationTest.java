5f81ed7a7c2a60171ae54fb036268bfcb5b6d3b8
ariel <alvarez.ariel.work@gmail.com>
2015-11-25 14:36:23 -0300

    Spy double for testing email notifications

--- src/test/java/ar/edu/unq/obj2/hotels/RoomReservationTest.java
+++ src/test/java/ar/edu/unq/obj2/hotels/RoomReservationTest.java
@@ -2,34 +2,44 @@ package ar.edu.unq.obj2.hotels;
 
 import ar.edu.unq.obj2.hotels.exceptions.DomainException;
 import ar.edu.unq.obj2.hotels.reservations.RoomReservation;
-import ar.edu.unq.obj2.hotels.search.Search;
-import ar.edu.unq.obj2.hotels.search.SearchResult;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.time.LocalDate;
-import java.util.Optional;
 
-import static ar.edu.unq.obj2.hotels.search.RoomFilterFactory.nameFilter;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 
 
 public class RoomReservationTest {
 
 
     HotelsFixtures hotelFixtures;
+    Room roomToReserve;
 
     @Before
     public void setUp(){
         hotelFixtures = new HotelsFixtures();
+        roomToReserve = hotelFixtures.hotelBarato.getRooms().stream().findFirst().get();
     }
 
     @Test(expected=DomainException.class)
-    public void notificationShouldBeSentAfterSuccessfulRegistration() {
-        Room room = hotelFixtures.hotelBarato.getRooms().stream().findFirst().get();
-        new RoomReservation(room, new DayRange(LocalDate.of(2015,12,24), LocalDate.of(2015,12,26)),hotelFixtures.passenger).register();
-        new RoomReservation(room, new DayRange(LocalDate.of(2015,12,15), LocalDate.of(2015,12,28)),hotelFixtures.passenger).register();
+    public void exceptionShoudBeThrownWhenAttemptingToReserveAnAlreadyReservedRoom() {
+
+        new RoomReservation(roomToReserve, new DayRange(LocalDate.of(2015,12,24), LocalDate.of(2015,12,26)),hotelFixtures.passenger).register();
+        new RoomReservation(roomToReserve, new DayRange(LocalDate.of(2015,12,15), LocalDate.of(2015,12,28)),hotelFixtures.passenger).register();
     }
+
+    @Test
+    public void notificationShouldBeSentAfterSuccessfulRegistration() {
+        ReservationsNotifierSpy notifier = new ReservationsNotifierSpy();
+        RoomReservation reservation =
+                new RoomReservation(roomToReserve, new DayRange(LocalDate.of(2015,12,24), LocalDate.of(2015,12,26)),hotelFixtures.passenger)
+                .usingNotifier(notifier);
+
+        reservation.register();
+
+        assertEquals(reservation, notifier.getReservation());
+    }
+
 
 }
